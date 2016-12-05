f192313bae2a1b2fdad7d4296f906dc411689a52
Lsong <song940@gmail.com>
2016-12-05 16:32:55 +0800

    remove scanner

--- public/js/ofo.js
+++ public/js/ofo.js
@@ -1,17 +1,5 @@
 ;(function(exports, undefined){
   
-  exports.recognize = function recognize(file){
-    var loading = document.getElementById('loading');
-    loading.style.display = 'fixed';
-    Tesseract
-    .recognize(file)
-    .progress(function(info){
-      info = info.status + '('+ Math.floor(parseFloat(info.progress) * 100) +'%)';
-      loading.innerText = info;
-    })
-    .then(function(data){
-      location.href = '/?q=' + data.text.match(/\d{6}/)[0];
-    });
-  }
+  console.log('欢迎使用「ofo」');
   
 })(window);
